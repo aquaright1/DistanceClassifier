5a296dc93099f6ad1b9777bd5273247a0ac3ffa6
Tim <readatontim@gmail.com>
2021-01-12 18:46:00 -0800

    some more

--- classifier.py
+++ classifier.py
@@ -2,11 +2,20 @@
 from sklearn.preprocessing import LabelEncoder
 
 class NNClassifier():
-    def __init__(self, ε = 0.0001):
+    def __init__(self, ε = 0.0001: float, threshold = .01: float):
         self.ε = ε
-        
+        self.threshold = threshold
+
     def fit(self,X,y):
+        '''
+        TODO:
+        implment the fit
+        '''
         pass
 
     def predict(self,X):
+        '''
+        TODO:
+            implement the prediciton
+        '''
         pass
