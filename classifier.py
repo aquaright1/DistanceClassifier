a92bfd0d6bc34619e14c7ecfc3a2507cd4dfba94
Tim <readatontim@gmail.com>
2021-01-12 18:44:16 -0800

    classifier beginining

--- classifier.py
+++ classifier.py
@@ -1,1 +1,12 @@
 ### Classifier ###
+from sklearn.preprocessing import LabelEncoder
+
+class NNClassifier():
+    def __init__(self, ε = 0.0001):
+        self.ε = ε
+        
+    def fit(self,X,y):
+        pass
+
+    def predict(self,X):
+        pass
