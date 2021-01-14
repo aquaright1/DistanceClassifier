945682878f1d24a63fe16f79218f3bd29671d11b
Tim <readatontim@gmail.com>
2021-01-13 23:15:01 -0800

    adding some code to fit

--- classifier.py
+++ classifier.py
@@ -1,6 +1,7 @@
 ### Classifier ###
 from sklearn.preprocessing import LabelEncoder
 from helpers import closest, shift, gamma_mle
+from collections import defaultdict
 
 class NNClassifier():
     def __init__(self, ε = 0.0001: float, threshold = .01: float):
@@ -12,7 +13,18 @@ class NNClassifier():
         TODO:
             implment the fit
         '''
-        pass
+        self.input_data = np.asarray(X)
+        self.encoder = LabelEncoder()
+        self.encoder.fit(y)
+
+        self.encoded_labels = self.encoder.transform(y)
+
+        self.params = np.zeros((len(self.encoder.classes_), 3))
+
+        self.distances = defaultdict(list)
+        for index, data in enumerate(self.input_data):
+            label = self.encoded_labels
+            distances[label].append(closest(data,self.input_data))
 
     def predict(self,X):
         '''
